7a580c4aacc5938612b23149e314fdaeb2114b2f
Haoyuan Tang <haoyuant@student.unimelb.edu.au>
2017-08-22 14:15:33 +1000

    Inital Commit

---
+++ Playground Ideas/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Playground Ideas
+//
+//  Created by Apple on 22/08/2017.
+//  Copyright © 2017 PlaygroundIdeasQuoll. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
