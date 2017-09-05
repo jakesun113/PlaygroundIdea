06316d9c129bdab31c3a566762e014b8c4e910bb
Haoyuan Tang <haoyuant@student.unimelb.edu.au>
2017-09-05 14:37:49 +1000

    fix api

---
+++ PlaygroundIdeasAPI/PlaygroundIdeasAPI.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundIdeasAPI.h
+//  PlaygroundIdeasAPI
+//
+//  Created by Apple on 01/09/2017.
+//  Copyright © 2017 PlaygroundIdeasQuoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlaygroundIdeasAPI.
+FOUNDATION_EXPORT double PlaygroundIdeasAPIVersionNumber;
+
+//! Project version string for PlaygroundIdeasAPI.
+FOUNDATION_EXPORT const unsigned char PlaygroundIdeasAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundIdeasAPI/PublicHeader.h>
+
+
