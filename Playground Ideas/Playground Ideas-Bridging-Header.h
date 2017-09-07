6de5781251575241e9c18496238ea346484a5a1e
Haoyuan Tang <haoyuant@student.unimelb.edu.au>
2017-09-07 23:11:35 +1000

    change ManualViewController to swift version

--- Playground Ideas/Playground Ideas-Bridging-Header.h
+++ Playground Ideas/Playground Ideas-Bridging-Header.h
@@ -2,6 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "ManualsViewController.h"
 #define V_S_W  [[UIScreen mainScreen] bounds].size.width
 #define V_S_H  [[UIScreen mainScreen] bounds].size.height
