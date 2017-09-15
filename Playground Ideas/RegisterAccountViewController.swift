ed04ef130e5a4f0a719ba96bcb2175258fe03c9a
tann5 <tann5@student.unimelb.edu.au>
2017-09-15 02:52:56 -0700

    register

---
+++ Playground Ideas/RegisterAccountViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegisterAccountViewController.swift
+//  Playground Ideas
+//
+//  Created by Khanh Tan Nguyen on 14/9/17.
+//  Copyright © 2017 PlaygroundIdeasQuoll. All rights reserved.
+//
+
+import UIKit
+
+class RegisterAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
